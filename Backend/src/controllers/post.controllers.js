f554e6e70b49f405f1a0ebf3b5e031367ca85247
name <123867179+agnijeetsharma@users.noreply.github.com>
2025-01-23 00:25:25 +0530

    completed controllers for the post

--- Backend/src/controllers/post.controllers.js
+++ Backend/src/controllers/post.controllers.js
@@ -5,7 +5,6 @@ import{ apiResponse} from "../utils/apiResponse"
 
 const createPost=asyncHandler(async(req,res)=>{
     const {title,description,postImage}=req.body
-    const user=req.user
     if(!title||!description){
         throw new apiError(401,"Post details are missing ")
     }
@@ -18,5 +17,27 @@ const createPost=asyncHandler(async(req,res)=>{
     return res.status(200).json(new apiResponse(200,post,"new Post created successfully"));
 })
 
+const postFeed=asyncHandler(async(req,res)=>{
+    const posts=await Post.find().sort({createdAt:-1}).limit(10)
+    return res.status(200).json(new apiResponse(200,posts,"Posts Feed"))
 
-export {createPost}
+})
+
+const updatePost=asyncHandler(async(req,res)=>{
+    const post=await Post.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    if(!post){
+        throw new apiError(404,"Post not found")
+        }
+        return res.status(200).json(new apiResponse(200,post,"Post updated successfully"))
+})
+
+const deletePost=asyncHandler(async(req,res)=>{
+    const post=await Post.findByIdAndDelete(req.params.id)
+    if(!post){
+        throw new apiError(404,"Post not found")
+        }
+        return res.status(200).json(new apiResponse(200,"Post deleted successfully"))
+})
+
+
+export {createPost,postFeed,deletePost}
