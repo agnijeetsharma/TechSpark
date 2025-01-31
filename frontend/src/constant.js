3093f00844e027c24660c8e788eb478d39e7d90c
name <123867179+agnijeetsharma@users.noreply.github.com>
2025-01-31 19:32:26 +0530

    changed base url for deloyment

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,2 +1,2 @@
-export const Base_URL="http://localhost:3000/api/v1/users"
+export const Base_URL="/api/v1/users"
 export const IMAGE_URL="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
