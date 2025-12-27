c01d5b5f5b976ec6dbf04393e7402c1d023f437a
agnijeetsharma <123867179+agnijeetsharma@users.noreply.github.com>
2025-12-27 17:38:40 +0530

    Update Base_URL for production environment

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,6 @@
 
-export const Base_URL =
-  location.hostname === "localhost" ? "http://localhost:3000/api/v1/users" : "/api/v1/users";
+// export const Base_URL =
+//   location.hostname === "localhost" ? "http://localhost:3000/api/v1/users" : "/api/v1/users";
 export const IMAGE_URL="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+export const Base_URL =
+  location.hostname === "localhost" ? "http://localhost:3000/api/v1/users" : "https://techspark-lfgr.onrender.com/api/v1/users";
